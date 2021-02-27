626cb97593211a6dffa3528e1ac93de39f3971d5
Abishai Prem <72638851+abishaip@users.noreply.github.com>
2021-02-27 16:00:35 -0600

    Update ebayBot.py

--- ebayBot.py
+++ ebayBot.py
@@ -1,44 +1,18 @@
 import discord
 from discord.ext import commands
-import json
 
-# Get configuration.json
-with open("configuration.json", "r") as config:
-    data = json.load(config)
-    token = data["token"]
-    prefix = data["prefix"]
+client = discord.Client()
 
 
-class Greetings(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        self._last_member = None
-
-# Intents
-intents = discord.Intents.default()
-intents.members = True
-
-bot = commands.Bot(prefix, intents = intents)
-
-# Load cogs
-initial_extensions = [
-    "Cogs.help",
-    "Cogs.ping"
-]
-
-print(initial_extensions)
-
-if __name__ == '__main__':
-    for extension in initial_extensions:
-        try:
-            bot.load_extension(extension)
-        except Exception as e:
-            print(f"Failed to load extension {extension}")
-
-@bot.event
+@client.event
 async def on_ready():
-    print(f"We have logged in as {bot.user}")
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name =f"{bot.command_prefix}help"))
-    print(discord.__version__)
+    bottest_channel = client.get_channel(815330331070693436)
+    await bottest_channel.send('hello')
 
-bot.run(token)
+@client.event
+async def on_message(message):
+    if message.content == 'bot test':
+        bottest_channel = client.get_channel(815330331070693436)
+        await bottest_channel.send('test')
+
+client.run('ODE1MzI4MjEwMzI0NTUzNzQ4.YDqzkQ.tfSw7TgMeWwsSGA5mPMbBimUZ-E')
